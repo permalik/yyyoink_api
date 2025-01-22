805aab2c5bc19ecd3170069af2db7e3e9d8a7031
permalik <permalik@protonmail.com>
2025-01-22 17:45:39 -0600

    feat: init accountConsumer

---
+++ YYYoinkAPI/AccountConsumer/accountConsumer.cs
@@ -0,0 +1,46 @@
+using Confluent.Kafka;
+using System;
+using System.Threading;
+
+class AccountConsumer
+{
+    static void Main(string[] args)
+    {
+        var config = new ConsumerConfig
+        {
+            BootstrapServers = "localhost:9094",
+            GroupId = "account_consumer_group",
+            AutoOffsetReset = AutoOffsetReset.Earliest
+        };
+
+        const string topic = "account_receipts";
+
+        CancellationTokenSource cts = new CancellationTokenSource();
+        Console.CancelKeyPress += (_, e) =>
+        {
+            e.Cancel = true;
+            cts.Cancel();
+        };
+
+        using (var consumer = new ConsumerBuilder<string, string>(config).Build())
+        {
+            consumer.Subscribe(topic);
+            try
+            {
+                while (true)
+                {
+                    var cr = consumer.Consume(cts.Token);
+                    Console.WriteLine($"consumed event from topic {topic}: key = null, value = {cr.Message.Value}");
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                Console.WriteLine("ctrl-c was pressed");
+            }
+            finally
+            {
+                consumer.Close();
+            }
+        }
+    }
+}
